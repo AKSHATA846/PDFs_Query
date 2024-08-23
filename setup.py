f37e0ae399a320d3a76c487c9246ba4eca6986f8
AKSHATA846 <akshatab846@gmail.com>
2024-08-23 10:48:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
  !pip install --upgrade pip
 !pip show streamlit
 !pip install streamlit
-#!pip install SpeechRecognition
+!pip install SpeechRecognition
 !pip install transformers sentence-transformers
 !pip install faiss-cpu
 
