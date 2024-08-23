b7a1f22f90312da64e1fc731e73cdec65cf65020
AKSHATA846 <akshatab846@gmail.com>
2024-08-23 11:00:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
- !pip install --upgrade pip
+!pip install --upgrade pip
 !pip show streamlit
 !pip install streamlit
 !pip install SpeechRecognition
 !pip install transformers sentence-transformers
 !pip install faiss-cpu
 !pip install RetrievalQA
+!pip install langchain
+!pip install streamlit gradio langchain SpeechRecognition pyttsx3 sentence-transformers
 
 
 
